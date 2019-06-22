e368aa31ccc45e5e815e07b7e68b86d8b73e4124
Jacob Herper <jacob@herper.uk>
2019-06-22 18:26:01 +0100

    1.6 to 1.11 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Fragment, useState } from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Statistics from "./components/Statistics"
+import Button from "./components/Button"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+	// save clicks of each button to own state
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
+
+	const addGood = () => setGood(good + 1)
+	const addNeutral = () => setNeutral(neutral + 1)
+	const addBad = () => setBad(bad + 1)
+
+	return (
+		<Fragment>
+			<h1>Give feedback</h1>
+			<Button onClick={() => addGood()}>good</Button>
+			<Button onClick={() => addNeutral()}>neutral</Button>
+			<Button onClick={() => addBad()}>bad</Button>
+
+			<h2>Statistics</h2>
+			<Statistics stats={{ good, neutral, bad }} />
+		</Fragment>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
