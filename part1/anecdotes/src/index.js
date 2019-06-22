ef57a2716d2666730016f3d1286e879e8aa374db
Jacob Herper <jacob@herper.uk>
2019-06-23 00:05:00 +0100

    part1 complete

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,10 +1,44 @@
-import React, { useState } from "react"
+import React, { Fragment, useState } from "react"
 import ReactDOM from "react-dom"
 
-const App = props => {
+const App = ({ anecdotes }) => {
 	const [selected, setSelected] = useState(0)
+	const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0])
 
-	return <div>{props.anecdotes[selected]}</div>
+	const selectAnecdote = () => {
+		setSelected(Math.floor(Math.random() * anecdotes.length))
+	}
+
+	const voteAnecdote = () => {
+		const votesList = [...votes]
+		votesList[selected]++
+		setVotes(votesList)
+	}
+
+	const mostVoted = () => votes.indexOf(Math.max(...votes))
+
+	return (
+		<Fragment>
+			<div>
+				<h1>Anecdote of the day</h1>
+				<p>{anecdotes[selected]}</p>
+				<p>
+					has {votes[selected]} vote
+					{votes[selected] !== 1 && `s`}
+				</p>
+			</div>
+			<button onClick={() => voteAnecdote()}>Vote</button>
+			<button onClick={() => selectAnecdote()}>Next anecdote</button>
+			<div>
+				<h2>Anecdote with the most votes</h2>
+				<p>{anecdotes[mostVoted()]}</p>
+				<p>
+					has {votes[mostVoted()]} vote
+					{votes[mostVoted()] !== 1 && `s`}
+				</p>
+			</div>
+		</Fragment>
+	)
 }
 
 const anecdotes = [
