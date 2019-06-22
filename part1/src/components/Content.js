7b11d5c30dbb9c8a87126c031fd7cc53f8f4a4be
Jacob Herper <jacob@herper.uk>
2019-06-22 10:47:57 +0100

    1.3, 1.4 and 1.5 completed

--- part1/src/components/Content.js
+++ part1/src/components/Content.js
@@ -2,18 +2,11 @@ import React from "react"
 
 import Part from "./Part"
 
-const Content = ({
-	part1,
-	part2,
-	part3,
-	exercises1,
-	exercises2,
-	exercises3,
-}) => (
+const Content = ({ parts }) => (
 	<React.Fragment>
-		<Part part={part1} exercises={exercises1} />
-		<Part part={part2} exercises={exercises2} />
-		<Part part={part3} exercises={exercises3} />
+		{parts.map((part, i) => (
+			<Part part={part.name} exercises={part.exercises} key={i} />
+		))}
 	</React.Fragment>
 )
 
--- part1/src/components/Total.js
+++ part1/src/components/Total.js
@@ -1,7 +1,12 @@
 import React from "react"
 
-const Total = ({ exercises }) => {
-	return <p>Number of exercises {exercises}</p>
+const Total = ({ parts }) => {
+	return (
+		<p>
+			Number of exercises{" "}
+			{parts.reduce((prev, next) => prev + next.exercises, 0)}
+		</p>
+	)
 }
 
 export default Total
